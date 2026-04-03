1bad49d194222a2cc58c9b286e129a17d30cc374
Mila Pacompia <mila@Milas-MacBook-Pro.local>
2026-04-03 15:50:22 +1100

    feat: update read file and slurm files

--- getLangRankRe.py
+++ getLangRankRe.py
@@ -1,6 +1,7 @@
 import sys
 import re
 import json
+import os
 
 from collections import Counter
 from mpi4py import MPI
@@ -10,83 +11,51 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-print("hello from process", rank, "of", size)
-# local
-#../files/mastodon-small.ndjson
 file_path = sys.argv[1]
+file_size = os.path.getsize(file_path)
 
-data = None
-lines = []
-number_lines = 0
-if rank == 0:
-    print('Rank 0 executing', file_path)
-    with open(file_path) as file_content:
-        lines = file_content.readlines()
-        print('lines length', len(lines))
-        number_lines = len(lines)
+# Each rank is responsible for a byte range of the file
+start_byte = rank * file_size // size
+end_byte = (rank + 1) * file_size // size
 
-number_lines = comm.bcast(number_lines, root=0)
-interval_size = int(number_lines / size)
-
-# interval of lines per each node
-# TODO: make this calculation in each node and assign more items to first n-1 process by default
-# currently for 4 processor and 10 rows -> 2, 2, 2, 4
-# it should be preferably 3, 3, 3, 1
-intervals = [
-    (
-        i * interval_size + int(bool(0)),
-        number_lines if i == size - 1 else (i + 1) * interval_size
-    ) for i in range(size)]
-
-interval_lines = []
-if rank == 0:
-    for start, end in intervals:
-        print('start', start)
-        print('end', end)
-        interval_lines.append(lines[start:end])
-
-node_lines = comm.scatter(interval_lines, root=0)
-
-# Executing for each node
-print('Rank ', rank, ' has ', len(node_lines), 'elems')
 # language_regex = r'("language")\s*(:)(\s*)(("([^"]*)")|\[.*\])(\s*),(\s*)'
 langs_regex = r'("langs"|"language")\s*(:)(\s*)(("([^"]*)")|\[\s*"[^"]*"(?:\s*,\s*"[^"]*")*\s*\])(\s*),(\s*)'
 string_array_regex = r'\[\s*"[^"]*"(?:\s*,\s*"[^"]*")*\s*\]'
 string_regex = r'"([^"]*)"'
-# string_array_regex = r''
 counter = Counter()
 
-for idx, node_line in enumerate(node_lines):
-    # TODO: join match_result and match_result_langs logic
-    match_result = re.search(langs_regex, node_line)
-    # match_result_langs = re.search(langs_regex, node_line)
-    if match_result:
-        value = match_result.group(4)
-        print('value', value)
-        if value:
-            array_string_value = re.search(string_array_regex, value)
-            string_value = re.search(string_regex, value)
-            array_strings = json.loads(value) if array_string_value else [string_value.group(1)]
-            # print('string_value', string_value.group(1))
-            counter.update(array_strings)
-    # elif match_result_langs:
-    #     value = match_result_langs.group(4)
-    #     if value:
-    #         array_strings = json.loads(value)
-    #         counter.update(array_strings)
-        # print('array_strings', array_strings)
+with open(file_path, 'rb') as f:
+    # If not rank 0, skip the partial line at the boundary to avoid double-counting
+    if start_byte > 0:
+        f.seek(start_byte - 1)
+        f.readline()  # advance to the start of the next complete line
+
+    while True:
+        pos = f.tell()
+        if pos >= end_byte:
+            break
+        line_bytes = f.readline()
+        if not line_bytes:
+            break
+        line = line_bytes.decode('utf-8', errors='replace')
+
+        match_result = re.search(langs_regex, line)
+        if match_result:
+            value = match_result.group(4)
+            if value:
+                array_string_value = re.search(string_array_regex, value)
+                string_value = re.search(string_regex, value)
+                array_strings = json.loads(value) if array_string_value else [string_value.group(1)]
+                counter.update(array_strings)
 
 
 all_counters = comm.gather(counter, root=0)
 
-print('all_counters', all_counters)
 if rank == 0:
     global_counter = Counter()
     for iter_counter in all_counters:
         global_counter.update(iter_counter)
-    print('ANSWER')
     most_common_lang = global_counter.most_common(10)
-    print('Language Used', 'Frequency of occurence (#posts)')
+    print('Language Used, Frequency of occurence (#posts)')
     for i in most_common_lang:
         print('{}, {}'.format(i[0], i[1]))
-    print('end')
--- utils.md
+++ utils.md
@@ -17,7 +17,6 @@ scp /Users/mila/comp90024/a1/comp90024_a1_team_11/getLangRank.py mpacompiamac@sp
 scp /Users/mila/comp90024/a1/comp90024_a1_team_11/getLangRankRe.slurm mpacompiamac@spartan.hpc.unimelb.edu.au:/home/mpacompiamac/a1
 
 
-
 # copies files directory into a1
 scp -r /Users/mila/comp90024/a1/files mpacompiamac@spartan.hpc.unimelb.edu.au:/home/mpacompiamac/a1
 ```
@@ -31,6 +30,74 @@ sbatch getLangRankRe.slurm
 ## Submitted batch job 23076094
 
 # In same directory it should displayed the output like slurm-23076094.out
+```
+
+## Execution in SPARTAN
+
+### Pre-requisites
+
+1. Copy the scripts from local to remote as follows
+
+```sh
+scp /Users/mila/comp90024/a1/comp90024_a1_team_11/slurm-scripts/getLangRankRe11.slurm mpacompiamac@spartan.hpc.unimelb.edu.au:/home/mpacompiamac/a1
+scp /Users/mila/comp90024/a1/comp90024_a1_team_11/slurm-scripts/getLangRankRe18.slurm mpacompiamac@spartan.hpc.unimelb.edu.au:/home/mpacompiamac/a1
+scp /Users/mila/comp90024/a1/comp90024_a1_team_11/slurm-scripts/getLangRankRe24.slurm mpacompiamac@spartan.hpc.unimelb.edu.au:/home/mpacompiamac/a1
+```
+
+2. Create symbolic links as detailed in the assignment specification.
+
+3. Execute next commands to send jobs
+
+```sh
+# Job name format is {DATASET}_{SIZE}_c{CONFIG}
+# DATASET: M (mastodon) or B (bluesky)
+# SIZE: S, M, L
+# CONFIG: 11 (1 node, 1 core), 18 (1 node, 8 core), 24 (2 node, 8 core)
+
+# Mastodon small file
+sbatch --job-name=M_S_c11 getLangRankRe11.slurm mastodon-small.ndjson 
+sbatch --job-name=M_S_c18 getLangRankRe18.slurm mastodon-small.ndjson
+sbatch --job-name=M_S_c24 getLangRankRe24.slurm mastodon-small.ndjson
+# Mastodon medium file
+sbatch --job-name=M_M_c11 getLangRankRe11.slurm mastodon-medium.ndjson
+sbatch --job-name=M_M_c18 getLangRankRe18.slurm mastodon-medium.ndjson
+sbatch --job-name=M_M_c24 getLangRankRe24.slurm mastodon-medium.ndjson
+# Mastodon large file
+sbatch --job-name=M_L_c11 getLangRankRe11.slurm mastodon-large.ndjson
+sbatch --job-name=M_L_c18 getLangRankRe18.slurm mastodon-large.ndjson
+sbatch --job-name=M_L_c24 getLangRankRe24.slurm mastodon-large.ndjson
+
+# Bluesky small file
+sbatch --job-name=B_S_c11 getLangRankRe11.slurm bluesky-small.ndjson
+sbatch --job-name=B_S_c18 getLangRankRe18.slurm bluesky-small.ndjson
+sbatch --job-name=B_S_c24 getLangRankRe24.slurm bluesky-small.ndjson
+# Bluesky medium file
+sbatch --job-name=B_M_c11 getLangRankRe11.slurm bluesky-medium.ndjson
+sbatch --job-name=B_M_c18 getLangRankRe18.slurm bluesky-medium.ndjson
+sbatch --job-name=B_M_c24 getLangRankRe24.slurm bluesky-medium.ndjson
+# Bluesky large file
+sbatch --job-name=B_L_c11 getLangRankRe11.slurm bluesky-large.ndjson
+sbatch --job-name=B_L_c18 getLangRankRe18.slurm bluesky-large.ndjson
+sbatch --job-name=B_L_c24 getLangRankRe24.slurm bluesky-large.ndjson
+```
+4. Write all ids
+```sh
+23477343, 23477366, 23477367, 23477368, 23477369, 23477373, # then large files
+23477380, 23477381, 23477382, 23477384, 23477385, 23477386 # then large files
+
+# inline
+23477343,23477366,23477367,23477368,23477369,23477373,23477380,23477381,23477382,23477384,23477385,23477386
+```
+5. Save statistic information in `test_sm_results.txt`
+```sh
+sacct -j 23477343,23477366,23477367,23477368,23477369,23477373,23477380,23477381,23477382,23477384,23477385,23477386 --format=JobName,Elapsed,CPUTime,MaxRSS,State --noheader > test_sm_results.txt
+```
+
+6. Plot results
+
+7. Run large files
+```sh
+sacct -j 23478499 --format=JobName,Elapsed,CPUTime,MaxRSS,State
 ```
 
 ## Local commands
